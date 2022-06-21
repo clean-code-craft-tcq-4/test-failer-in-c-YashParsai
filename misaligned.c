db874a3b2d02341b6dfc567a15d8a1377c011e3c
Yash <yash.parsai@in.bosch.com>
2022-06-21 05:26:32 -0400

    changes in misaligned

--- misaligned.c
+++ misaligned.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
-#include "misaligned.h"
-#include "test_colorMap.h"
+#include <string.h>
+
+void printStringToConsole(char* string)
+{
+    printf("%s",string);
+}
+
+// This function will build string for color map for providedd index.
+// param[in]: i = index of majorColor
+// param[in]:j = index of minorColor
+// param[in]: string = to hold the content of the colormap in string format.
+// param[out]: pointer to string of colorCode.
+void  buildColorMapString(int i, int j, char* string)
+{
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+    sprintf(string,"%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+}
 
 int printColorMap(void)
 {
@@ -20,23 +37,25 @@ int printColorMap(void)
     return i * j;
 }
 
-// This function will build string for color map for providedd index.
-// param[in]: i = index of majorColor
-// param[in]:j = index of minorColor
-// param[in]: string = to hold the content of the colormap in string format.
-// param[out]: pointer to string of colorCode.
-void  buildColorMapString(int i, int j, char* string)
+void test_printColorMap(void)
 {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    
-    sprintf(string,"%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    int i = 0, j = 0;
+    char string[30];
+    char string_test[30];
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5; j++)
+        {
+            sprintf(string_test,"%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            buildColorMapString(i,j,string);
+            assert(!strcmp(string,string_test));
+        }
+    }
 }
 
-void printStringToConsole(char* string)
-{
-    printf("%s",string);
-}
 
 int main()
 {
--- misaligned.h
+++
@@ -1,5 +0,0 @@
-int printColorMap(void);
-void  buildColorMapString(int i, int j, char* string);
-void printStringToConsole(char* string);
-
-
