337dde89e089c8920e45bea28a7e23fe827c0166
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-06-21 18:32:54 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
-#include "defines.h"
-#ifdef unit_test_enable
 #include "test_alerter.h"
-#else
-#include "alerter.h"
-#endif
+
 
 int alertFailureCount = 0;
 
 void alertInCelcius(float farenheit) 
 {
     float celcius = (farenheit - 32) * 5 / 9;
-#ifdef unit_test_enable
     int returnCode = networkAlertStub(celcius);
-#else
-    int returnCode = networkAlert(celcius);
-#endif
+
     if (returnCode != 200)
     {
         // non-ok response is not an error! Issues happen in life!
