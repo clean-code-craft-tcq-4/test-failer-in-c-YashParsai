7d639c61bd3f20831479742927c10b902047ba33
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-06-29 14:23:19 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -2,29 +2,31 @@
 #include <assert.h>
 
 #define MAX_THRESHOLD 200
+#define ok 200
+#define not_ok 500
 
 int alertFailureCount = 0;
 
 int stub_networkAlert(float celcius)
 {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
     if(celcius <= MAX_THRESHOLD)
     {
-        return 200;
+        return ok;
     }
     else
     {
-        return 500;
+        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+        return not_ok;
     }
 }
 
 /**
  * This stub will convert temperature units from farenheit to celcius
- * param[in] - temperature in farenheit
- * param[out] - temperature in celcius
+ * @param[in]  - temperature in farenheit
+ * @param[out] - temperature in celcius
  */
 float stub_convertFtoC( float farenheit)
 {
@@ -36,7 +38,7 @@ void alertInCelcius(float farenheit)
     float celcius       = stub_convertFtoC(farenheit);
     int   returnCode    = stub_networkAlert(celcius);
     
-    if (returnCode != 200)
+    if (returnCode == not_ok)
     {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -44,6 +46,9 @@ void alertInCelcius(float farenheit)
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 1;
     }
+
+    assert((returnCode == ok) && (celcius <= MAX_THRESHOLD));
+    assert((returnCode == not_ok) && (celcius > MAX_THRESHOLD));
     
 }
 
@@ -51,7 +56,10 @@ int main()
 {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    alertInCelcius(300.8);
+    alertInCelcius(401.9);
+    
     printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well (maybe!)\n");
+    printf("All is well \n");
     return 0;
 }
